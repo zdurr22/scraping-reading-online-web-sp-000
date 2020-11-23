607fa09654387cb8539f496cfd7190a3ca6e264d
Zachary Durr <zdurr22@gmail.com>
2020-11-23 03:56:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,9 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text
+
+courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip
+end
